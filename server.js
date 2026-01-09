8ce7c8b757e2ddd867c3918d6651cf964cff7cb2
mdshahanwaz7 <hasmatikhatoon77@gmail.com>
2026-01-09 19:10:17 +0530

    updated

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
-import dotenv  from "dotenv";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routes/user.js";
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",           
-      "https://vercel.com/md-shahanwazs-projects/login-assignment" 
-    ],
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+  
+
+
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://login-assignment-eight.vercel.app"
+  ],
+  credentials: true,
+}));
+
+
 
 app.use(express.json());
 
+
 app.use("/api/user", userRouter);
 
-await mongoose.connect(process.env.MONGO_URL);
 
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Mongo error:", err));
 
 
 const PORT = process.env.PORT || 1000;
-app.listen(PORT, () => console.log("Server running"));
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
+
