85bd1bef7e8aff711991a90e9c1211fece7da5cc
mdshahanwaz7 <hasmatikhatoon77@gmail.com>
2026-01-09 20:20:53 +0530

    updated

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from 'express'
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+import {config} from "dotenv"
 import userRouter from "./routes/user.js";
 
-dotenv.config();
+config({ path: ".env" });
+
 
 const app = express();
 
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://login-assignment-eight.vercel.app"
+  ],
+  credentials: true
+}));
   
 
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 
 
